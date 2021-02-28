344d68698330fe7b4a2f8557851733f7be1f8128
Damian Linek <damianlinekk@gmail.com>
2021-02-28 17:36:23 +0100

    Init commit

---
+++ MasnaSzama/src/main/java/com/project/MasnaSzama/MasnaSzamaApplication.java
@@ -0,0 +1,22 @@
+package com.project.MasnaSzama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MasnaSzamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasnaSzamaApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s! Masna Szama wita", name);
+	}
+
+}
---
+++ MasnaSzama/src/test/java/com/project/MasnaSzama/MasnaSzamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MasnaSzama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasnaSzamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
