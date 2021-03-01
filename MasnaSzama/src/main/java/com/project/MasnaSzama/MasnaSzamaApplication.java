53a9c6e522adbb79417d189184f7a5c4d94091c6
Damian Linek <damianlinekk@gmail.com>
2021-03-01 19:28:43 +0100

    angular view added

--- MasnaSzama/src/main/java/com/project/MasnaSzama/MasnaSzamaApplication.java
+++ MasnaSzama/src/main/java/com/project/MasnaSzama/MasnaSzamaApplication.java
@@ -2,15 +2,14 @@ package com.project.MasnaSzama;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class MasnaSzamaApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(MasnaSzamaApplication.class, args);
 	}
 
