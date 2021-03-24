8b876f8b264aef53612dbb5209b060e1dfc1ecae
D-Jonczyk <danieljonczyk.w@gmail.com>
2021-03-24 12:55:59 +0100

    update: prompt for admin privileges only if calling net start

--- MasnaSzama/startsql.bat
+++ MasnaSzama/startsql.bat
@@ -1,15 +1,16 @@
 @echo off
 
-REM elevate to admin
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
 for /F "tokens=3 delims=: " %%H in ('sc query "MySQL80" ^| findstr "        STATE"') do (
   IF /I "%%H" NEQ "RUNNING" (
+
+   REM elevate to admin
+   set "params=%*"
+   cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
    @echo MySQL80 service is not running, starting now...
    net start "MySQL80"
+   exit
   ) ELSE (
    @echo MySQL80 is up and running, proceeding...
   )
 )
-exit
