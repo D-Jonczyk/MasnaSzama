6a190820a35702977ef5ea27b91c877f12ceaf3c
Damian Linek <damianlinekk@gmail.com>
2021-03-24 15:19:36 +0100

    restaurant DTO

--- MasnaSzama/src/main/java/com/project/MasnaSzama/Controller/RestaurantController.java
+++ MasnaSzama/src/main/java/com/project/MasnaSzama/Controller/RestaurantController.java
@@ -1,25 +1,28 @@
 package com.project.MasnaSzama.Controller;
 
+import com.project.MasnaSzama.DTO.RestaurantSummaryDTO;
 import com.project.MasnaSzama.Model.Restaurant.Restaurant;
 import com.project.MasnaSzama.Repository.Restaurant.RestaurantRepository;
+import com.project.MasnaSzama.Service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping(path = "/restaurant")
 public class RestaurantController {
 
-    @Autowired
-    private RestaurantRepository restaurantRepository;
+    private final RestaurantService restaurantService;
+
+    public RestaurantController(RestaurantService restaurantService) {
+
+        this.restaurantService = restaurantService;
+    }
 
     @GetMapping(path = "/getbycity")
-    public List<Restaurant> getRestaurantsByCity(@RequestParam String city){
+    public List<RestaurantSummaryDTO> getRestaurantByCity(@RequestParam String city){
 
-        List<Restaurant> restaurants = restaurantRepository.getRestaurantByCity(city);
-        for (var r : restaurants) {
-            //System.out.println(r.getName());
-        }
-        return restaurants;
+        return restaurantService.getRestaurantByCity(city);
     }
 }
