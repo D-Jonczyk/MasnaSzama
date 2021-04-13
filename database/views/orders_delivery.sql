a457eeabe9231a48de4bd2827ee7bccf2e56c501
D-Jonczyk <danieljonczyk.w@gmail.com>
2021-04-13 16:18:34 +0200

    add: orders_delivery view for mysql db

---
+++ database/views/orders_delivery.sql
@@ -0,0 +1,19 @@
+DROP VIEW IF EXISTS orders_delivery;
+
+CREATE VIEW orders_delivery AS
+SELECT o.order_id, 
+	co.person_id AS 'courier_id', 
+	o.order_price, 
+	r.name AS 'resto_name', 
+    CONCAT(ad.street, ', ', ad.number) AS 'customer_address',
+    cu.phone_number,
+	DATE_FORMAT(o.ordered_time, "%T") AS 'ordered_time', 
+	DATE_FORMAT(o.desired_delivery_time, "%T") AS 'desired_delivery_time'
+FROM couriers_orders co
+JOIN order_ o ON o.order_id = co.order_id
+JOIN status_ s ON o.status_id = s.status_id
+JOIN courier c ON co.person_id = c.person_id
+JOIN restaurant r ON o.restaurant_id = r.restaurant_id
+JOIN customer cu ON o.customer_id = cu.person_id
+JOIN address ad ON cu.address_id = ad.address_id
+WHERE s.name LIKE 'delivery';
