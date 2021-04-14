3f2149976c62b4ecd3b1ac87b53b782019af2fff
D-Jonczyk <danieljonczyk.w@gmail.com>
2021-04-14 15:39:21 +0200

    show schedule

--- MasnaSzama/src/main/view/MasnaSzama/src/app/courier-panel/Schedule/show-schedule/show-schedule.component.ts
+++ MasnaSzama/src/main/view/MasnaSzama/src/app/courier-panel/Schedule/show-schedule/show-schedule.component.ts
@@ -1,7 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {FaIconLibrary} from '@fortawesome/angular-fontawesome';
 import {faPlayCircle} from '@fortawesome/free-regular-svg-icons';
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-show-schedule-content',
+  template: `
+    <div class="modal-header">
+      <h4 class="modal-title">Hi there!</h4>
+      <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+    <div class="modal-body">
+      <p>Hello, {{name}}!</p>
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+export class ShowScheduleContent {
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) {}
+}
 
 @Component({
   selector: 'app-show-schedule',
@@ -29,11 +53,15 @@ export class ShowScheduleComponent implements OnInit {
   months: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   days: string[] = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   numbers: number[] = [1, 2, 3, 4, 5, 6, 7];
-  constructor(public route: ActivatedRoute, private library: FaIconLibrary) {
+  constructor(public route: ActivatedRoute, private library: FaIconLibrary, private modalService: NgbModal) {
     library.addIcons(faPlayCircle);
   }
 
   ngOnInit(): void {
   }
 
+  open(): void {
+    const modalRef = this.modalService.open(ShowScheduleContent);
+    modalRef.componentInstance.name = 'World';
+  }
 }
